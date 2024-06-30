806d000b237409f975811069f29f09697ec5693e
Evgenia Larina <larsik29@yandex.ru>
2024-06-30 23:29:28 +0300

    README changed

--- src/main/java/ru/netology/services/FreelancerService.java
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -1,2 +1,23 @@
-package ru.netology.services;public class FreelancerService {
+package ru.netology.services;
+
+public class FreelancerService {
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) {
+                // Месяц отдыха
+                money -= expenses;
+                money -= money / 3;
+                count++;
+            } else {
+                // Месяц работы
+                money += income;
+                money -= expenses;
+            }
+        }
+        return count;
+    }
 }
+
--- src/test/java/ru/netology/services/FreelancerServiceTest.java
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -1,2 +1,30 @@
-package ru.netology.services;public class FreelancerServiceTest {
+package ru.netology.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FreelancerServiceTest {
+
+    @Test
+    void shouldCalculateRestMonthsExample1() {
+        FreelancerService service = new FreelancerService();
+        int income = 10000;
+        int expenses = 3000;
+        int threshold = 20000;
+        int expected = 3;
+
+        int actual = service.calculate(income, expenses, threshold);
+    }
+
+    @Test
+    void shouldCalculateRestMonthsExample2() {
+        FreelancerService service = new FreelancerService();
+        int income = 100000;
+        int expenses = 60000;
+        int threshold = 150000;
+        int expected = 2;
+
+        int actual = service.calculate(income, expenses, threshold);
+    }
 }
